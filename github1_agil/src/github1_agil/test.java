454dff075ff3a5cc077dd7e6942712113433dbb7
DELL <DELL@DELL-PC>
2020-02-27 15:18:10 +0530

    s

--- github1_agil/src/github1_agil/test.java
+++ github1_agil/src/github1_agil/test.java
@@ -1,10 +1,10 @@
 package github1_agil;
 
-public class main 
+public class test
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("jyo");
+		System.out.println("hello");
 		
 	}
 
